6ef25e303fe140b646b9423204bffebe7d480444
blusky-cloud <sspitzer@pdx.edu>
2021-05-17 14:00:41 -0700

    update first_contact and comment

--- source/live/first_contact.py
+++ source/live/first_contact.py
@@ -3,19 +3,45 @@ import requests
 import json
 from requests_toolbelt.utils import dump
 
+# This is the name for the file where the data is saved
+# Also, text after a "#" sign won't be evaluated as code, they're comments
+data_file_name = 'datafile_title.txt'
+
+
+# This function saves a json 'list' to a file
+# if you change the file name, a new file will be written
+def save_json_to_file(data_to_write):
+	with open(data_file_name, 'w') as outfile:
+		json.dump(data_to_write, outfile, indent=2)
+	outfile.close()
+
 
 url = 'https://api.usaspending.gov/api/v2/recipient/state/'
 
-resp = requests.get(url)
-print("get req made")
+# This is where we make a GET request to the server at the specified url
+response_from_server = requests.get(url)
+print("REQUEST TO SERVER HAS BEEN MADE")
 
-something = resp.json()
-res_bytes = json.dumps(something).encode('utf-8')
+# This line extracts the JSON data out of the message we got from the server
+json_from_server = response_from_server.json()
+
+# This encodes it as a bytestream
+res_bytes = json.dumps(json_from_server).encode('utf-8')
+# This loads the bytestream into a json_object
 json_object = json.loads(res_bytes)
+# This converts the json_object to a form that can be printed to the terminal (string)
 json_formatted_str = json.dumps(json_object, indent=2)
+# Print the nicely formatted json data to the terminal
+print("NOW PRINTING NICELY FORMATTED JSON WE RECEIVED FROM THE SERVER:")
 print(json_formatted_str)
+print("NICELY FORMATTED JSON IS DONE PRINTING")
 
+# here is the function call to save data to a file
+save_json_to_file(json_from_server)
+print("DATA SAVED TO FILE")
 
-data = dump.dump_all(resp)
+print("NOW PRINTING ENTIRE RESPONSE:")
+# This will print the entire response from the server, not just the JSON
+data = dump.dump_all(response_from_server)
 print(data.decode('utf-8'))
 files = data.decode('utf-8')
