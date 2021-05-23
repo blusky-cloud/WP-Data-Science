666744b0015358be2ecf2aa82521a5b4182b3b04
blusky-cloud <sspitzer@pdx.edu>
2021-05-23 14:52:14 -0700

    first_cfda works

--- source/live/first_cfda.py
+++ source/live/first_cfda.py
@@ -2,6 +2,7 @@ import csv
 import requests
 import json
 from requests_toolbelt.utils import dump
+from time import sleep
 
 # This is the name for the file where the data is saved, as well as the path
 # The path specifies that the .txt files should be stored in the data/testing folder
@@ -10,8 +11,9 @@ from requests_toolbelt.utils import dump
 # If you're running this locally, just keep the name in the save_json_to_file function
 # As "data_file_name" and it will save in whatever folder the python script is stored
 data_file_path = '../../data/testing/'
-data_file_name = 'cfda_10-664_by_award.txt'
-data_file = data_file_path + data_file_name # will error without data and testing folders
+data_file_name = '10-664_whole_cfda_test.txt'
+data_file = data_file_path + data_file_name  # will error without data and testing folders
+
 
 # This function saves a json 'list' to a file
 # if you change the file name, a new file will be written
@@ -19,6 +21,46 @@ def save_json_to_file(data_to_write):
 	with open(data_file, 'w') as outfile:
 		json.dump(data_to_write, outfile, indent=2)
 	outfile.close()
+
+
+def Append_json_to_file(data_to_write):
+	with open(data_file, 'a') as outfile:
+		json.dump(data_to_write, outfile, indent=2)
+	outfile.close()
+
+
+def JSON_ify(response_from_server):
+	# This line extracts the JSON data out of the message we got from the server
+	json_from_server = response_from_server.json()
+	# This encodes it as a bytestream
+	res_bytes = json.dumps(json_from_server).encode('utf-8')
+	# This loads the bytestream into a json_object
+	json_object = json.loads(res_bytes)
+	# This converts the json_object to a form that can be printed to the terminal (string)
+	json_formatted_str = json.dumps(json_object, indent=2)
+	# Print the nicely formatted json data to the terminal
+	print("NOW PRINTING NICELY FORMATTED JSON WE RECEIVED FROM THE SERVER:")
+	print(json_formatted_str)
+	print("NICELY FORMATTED JSON IS DONE PRINTING")
+	return json_from_server
+
+
+def POST(url, body):
+	headers = {'Content-Type': 'application/json'}
+	payload = body
+	resp = requests.post(url, headers=headers, json=payload)
+	print("REQUEST TO SERVER HAS BEEN MADE")
+	return resp
+
+
+def POST_for_new_page(url, body, page):
+	headers = {'Content-Type': 'application/json'}
+	body['page'] = page
+	payload = body
+	resp = requests.post(url, headers=headers, json=payload)
+	print("REQUEST TO SERVER HAS BEEN MADE")
+	return resp
+
 
 url_base = 'https://api.usaspending.gov'
 state_1_url = '/api/v2/recipient/state/'
@@ -55,7 +97,8 @@ body = {
 	"fields": [
 		"Award ID", "Recipient Name", "Start Date", "End Date", "Award Amount", "Description", "def_codes",
 		"COVID-19 Obligations", "COVID-19 Outlays", "Awarding Agency", "Awarding Sub Agency", "Award Type",
-		"recipient_id", "prime_award_recipient_id", "CFDA Number"
+		"recipient_id", "prime_award_recipient_id", "CFDA Number", "Place of Performance State Code",
+		"Place of Performance Country Code", "Place of Performance Zip5"
 	],
 	"page": 1,
 	"limit": 100,
@@ -63,12 +106,61 @@ body = {
 	"order": "desc",
 	"subawards": False
 }
+
 url = url_base + spend_by_award
-headers = {'Content-Type': 'application/json'}
-payload = body
 
-response_from_server = requests.post(url, headers=headers, json=payload)
+response_from_server = POST(url, body)
+downloading = True
+page_to_request = 1
 
+while downloading:
+	if response_from_server.status_code == 200:
+		response_from_server = POST_for_new_page(url, body, page_to_request)
+		json_response = JSON_ify(response_from_server)
+
+		response_page = json_response['page_metadata']['page']
+		has_next_page = json_response['page_metadata']['hasNext']
+		print("response page: ")
+		print(response_page)
+		print(has_next_page)
+
+		print("NOW PRINTING ENTIRE RESPONSE:")
+		# This will print the entire response from the server, not just the JSON
+		data = dump.dump_all(response_from_server)
+		print(data.decode('utf-8'))
+		files = data.decode('utf-8')
+		# This is where we make a GET request to the server at the specified url
+
+		json_response = JSON_ify(response_from_server)
+
+		# here is the function call to both print to terminal and save data to a file
+		if response_page == 1:
+			save_json_to_file(json_response)
+			print("DATA SAVED TO FILE")
+		else:
+			Append_json_to_file(json_response)
+			print("DATA APPENDED TO FILE")
+
+		if has_next_page:
+			downloading = True
+			page_to_request += 1
+		else:
+			downloading = False
+		sleep(1)
+
+	# if things did not go smoothly
+	else:
+
+		print("NOW PRINTING ENTIRE RESPONSE AS ERROR:")
+		print(type(response_from_server))
+		# This will print the entire response from the server, not just the JSON
+		data = dump.dump_all(response_from_server)
+		print(data.decode('utf-8'))
+		files = data.decode('utf-8')
+		downloading = False
+		sleep(1)
+
+'''
 print("NOW PRINTING ENTIRE RESPONSE:")
 # This will print the entire response from the server, not just the JSON
 data = dump.dump_all(response_from_server)
@@ -76,25 +168,12 @@ print(data.decode('utf-8'))
 files = data.decode('utf-8')
 # This is where we make a GET request to the server at the specified url
 
-print("REQUEST TO SERVER HAS BEEN MADE")
+json_response = JSON_ify(response_from_server)
 
-# This line extracts the JSON data out of the message we got from the server
-json_from_server = response_from_server.json()
-
-# This encodes it as a bytestream
-res_bytes = json.dumps(json_from_server).encode('utf-8')
-# This loads the bytestream into a json_object
-json_object = json.loads(res_bytes)
-# This converts the json_object to a form that can be printed to the terminal (string)
-json_formatted_str = json.dumps(json_object, indent=2)
-# Print the nicely formatted json data to the terminal
-print("NOW PRINTING NICELY FORMATTED JSON WE RECEIVED FROM THE SERVER:")
-print(json_formatted_str)
-print("NICELY FORMATTED JSON IS DONE PRINTING")
-
-# here is the function call to save data to a file
-save_json_to_file(json_from_server)
-print("DATA SAVED TO FILE")
-
+# here is the function call to both print to terminal and save data to a file
+if (response_page == 1):
+	save_json_to_file(json_response)
+	print("DATA SAVED TO FILE")
+'''
 
 print("DONE")
