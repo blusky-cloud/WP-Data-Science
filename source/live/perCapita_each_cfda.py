87f677ab93fc3023d38645ca614ffdbaa124f4ae
blusky-cloud <sspitzer@pdx.edu>
2021-05-27 12:54:40 -0700

    per capita started

--- source/live/perCapita_each_cfda.py
+++ source/live/perCapita_each_cfda.py
@@ -52,6 +52,16 @@ def append_sum_strs(qty, addend):
 	return str(result)
 
 
+def divide_strs(numerator, denominator):
+	print(numerator, "/", denominator)
+	n = round(float(numerator), 6)
+	d = round(float(denominator.replace(',', '')))
+	print("converted : ", n, "/", d)
+	per_cap = round(n/d, 3)
+	print("result (rounded) :", per_cap, ", as str: ", str(per_cap))
+	return str(per_cap)
+
+
 def tally_state_totals(cfda_csv_list, state_list):
 	for row in cfda_csv_list:
 		if row[-4] == 'USA':
@@ -60,6 +70,37 @@ def tally_state_totals(cfda_csv_list, state_list):
 				if state[2] == row[-5]:
 					state[4] = append_sum_strs(state[4], row[5])
 	return state_list
+
+
+def calculate_per_capita_spending(state_list):
+
+	for state in state_list[1::]:
+		state[5] = divide_strs(state[4], state[1])
+	return state_list
+
+
+def get_per_cap(state):
+	return state[5]
+
+def rank_states_per_cap_spending(state_list):
+	headers = state_list[0]
+	print(headers)
+	state_list.sort(key=lambda cap: cap[5], reverse=True)
+	for state in state_list:
+		state[6] = str(state_list.index(state) + 1)
+	state_list[0] = headers
+	return state_list
+
+
+def Write_CSVList(lst, file):
+	with open(file, 'w', newline='') as csvfile:
+		# create the csv writer object
+		csv_writer = csv.writer(csvfile)
+		# Counter variable used for writing
+		# headers to the CSV file
+		for res in lst:
+			csv_writer.writerow(res)
+	csvfile.close()
 
 
 # --------------------------------------------------------------------------------
@@ -101,7 +142,10 @@ for state in per_cap_list[1::]:
 print(per_cap_list)
 
 per_cap_list = tally_state_totals(cfda_file_contents, per_cap_list)
+per_cap_list = calculate_per_capita_spending(per_cap_list)
+per_cap_list = rank_states_per_cap_spending(per_cap_list)
 
+Write_CSVList(per_cap_list, set_state_CFDA_filename(cfda_array[cfda_index]))
 
 print("--------------------------------------------------")
 print(per_cap_list)
