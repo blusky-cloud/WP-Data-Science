7466ddfd1a0f9ecfe0b496b16df3520ebede2c17
blusky-cloud <sspitzer@pdx.edu>
2021-06-03 20:49:24 -0700

    washington rankings

--- source/live/perCapita_each_cfda.py
+++ source/live/perCapita_each_cfda.py
@@ -53,44 +53,53 @@ def append_sum_strs(qty, addend):
 
 
 def divide_strs(numerator, denominator):
-	#print(numerator, "/", denominator)
+	#  print(numerator, "/", denominator)
 	n = round(float(numerator), 6)
 	d = round(float(denominator.replace(',', '')))
-	#print("converted : ", n, "/", d)
+	#  print("converted : ", n, "/", d)
 	per_cap = round(n/d, 3)
-	#print("result (rounded) :", per_cap, ", as str: ", str(per_cap))
+	#  print("result (rounded) :", per_cap, ", as str: ", str(per_cap))
 	return str(per_cap)
 
 
 def tally_state_totals(cfda_csv_list, state_list):
 	for row in cfda_csv_list:
-		if row[-4] == 'USA':
+		if row[-4] == 'USA':  # index in sublist of country column
 			# print("state: ", row[-5], " value: ", row[5])
 			for state in state_list:
-				if state[2] == row[-5]:
-					state[4] = append_sum_strs(state[4], row[5])
+				if state[2] == row[-5]:  # state abbreviation from populations list, and state abbreviation column from cfda csv
+					state[4] = append_sum_strs(state[4], row[5])  # because data is stored as strings
 	return state_list
 
 
 def calculate_per_capita_spending(state_list):
 	for state in state_list:
-		state[5] = divide_strs(state[4], state[1])
+		state[6] = divide_strs(state[4], state[1])
 	return state_list
 
 
-def get_per_cap(state):
-	return float(state[5])
+def get_per_cap(s):
+	return float(s[6])  # 5th column is per cap spending
+
+
+def get_total_spending(s):
+	return float(s[4])  # 4th column is total spending
+
 
 def rank_states_per_cap_spending(state_list):
-	list_to_check = []
-	new_list = []
-	compare = 0.00
-	# list2 = sorted(state_list, key=get_per_cap)
-	# print(list2)
 	state_list.sort(key=get_per_cap, reverse=True)
 	# print("sorted: ", state_list)
 	for s in state_list:
-		s[6] = str(state_list.index(s) + 1)
+		s[7] = str(state_list.index(s) + 1)
+	# print("state list: ", state_list)
+	return state_list
+
+
+def rank_states_total_spending(state_list):
+	state_list.sort(key=get_total_spending, reverse=True)
+	# print("sorted: ", state_list)
+	for s in state_list:
+		s[5] = str(state_list.index(s) + 1)
 	# print("state list: ", state_list)
 	return state_list
 
@@ -112,31 +121,32 @@ def Write_CSVList(lst, file):
 
 cfda_index = 0
 cfda_array = Read_CFDA_Nums_From_File(cfda_list_file)
-#print(cfda_file_contents)
+#  print(cfda_file_contents)
 print("--------------------------------------------------")
 
-#print(cfda_file_contents[1][-4])
-#print(len(cfda_file_contents))
+#  print(cfda_file_contents[1][-4])
+#  print(len(cfda_file_contents))
 
-#if cfda_file_contents[1][-4] == 'USA':
-	#print("USA id")
-	#print(cfda_file_contents[1][-5])
+#  if cfda_file_contents[1][-4] == 'USA':
+	#  print("USA id")
+	#  print(cfda_file_contents[1][-5])
 
-
+'''
 index = 0
 while index < len(cfda_array):
-#while index < 1:
 	print(cfda_array[index])
 	cfda_file_contents = read_csv(set_CFDA_filename(cfda_array[index]))
 	pops_list = read_csv(state_pops_file)
 	per_cap_list = pops_list
 
 	spent = '0.00'
+	spent_rank = '0'
 	per_cap_spent = '0.00'
 	rank = '0'
 
 	per_cap_list[0].append('CFDA')
 	per_cap_list[0].append('Total Spending')
+	per_cap_list[0].append('Total Spending Rank')
 	per_cap_list[0].append('Per Capita Spending')
 	per_cap_list[0].append('Per Capita Rank')
 	per_cap_data = per_cap_list[1::]
@@ -144,15 +154,18 @@ while index < len(cfda_array):
 	for state in per_cap_data:
 		state.append(cfda_array[index])
 		state.append(spent)
+		state.append(spent_rank)
 		state.append(per_cap_spent)
 		state.append(rank)
 
 	per_cap_data = tally_state_totals(cfda_file_contents, per_cap_data)
 	per_cap_data = calculate_per_capita_spending(per_cap_data)
 	per_cap_data = rank_states_per_cap_spending(per_cap_data)
+	per_cap_data = rank_states_total_spending(per_cap_data)
 
 	final_list = per_cap_list[:1:] + per_cap_data
 
 	Write_CSVList(final_list, set_state_CFDA_filename(cfda_array[index]))
 	index += 1
-print("--------------------------------------------------")
+'''
+print("---------------perCapita_each_cfda Operations Complete------------------------")
---
+++ source/live/washington_analysis.py
@@ -0,0 +1,47 @@
+import perCapita_each_cfda as p
+
+state_rankings_file_path = '../../data/analysis/State/'
+state_name = 'Washington'
+state_name_abbrev = 'WA'
+state_cfda_rankings_file = state_rankings_file_path + state_name + '_' + state_name_abbrev + '_rankings.csv'
+
+
+def find_washington_info(state_list, w_list):
+	new_row = []
+	for row in state_list:
+		if row[2] == state_name_abbrev:  # if state abbrev. column is washinton
+			new_row.append(row[3])
+			new_row.append(row[4])
+			new_row.append(row[5])
+			new_row.append(row[6])
+			new_row.append(row[7])
+			print("new_row: ", new_row)
+			w_list.append(new_row)
+	return w_list
+
+
+# -------------------------------------------------
+#     DO STUFF HERE
+# -------------------------------------------------
+print("Performing Analysis for Washington...")
+
+cfda_index = 0
+cfda_array = p.Read_CFDA_Nums_From_File(p.cfda_list_file)
+#  print(cfda_file_contents)
+print("--------------------------------------------------")
+washington_list = [['CFDA', 'Total Spending', 'Total Spending Rank', 'Per Capita Spending', 'Per Capita Spending Rank']]
+print(washington_list[0])
+index = 0
+
+while index < len(cfda_array):
+	print(cfda_array[index])
+	cfda_state_contents = p.read_csv(p.set_state_CFDA_filename(cfda_array[index]))
+	#  print(cfda_state_contents)
+
+	washington_list = find_washington_info(cfda_state_contents, washington_list)
+	#  print("created washington row: ", washington_list)
+	index += 1
+
+print("--------------------------------------------------")
+print("washington list: ", washington_list)
+p.Write_CSVList(washington_list, state_cfda_rankings_file)
